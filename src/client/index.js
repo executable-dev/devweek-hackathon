e4344381ea026b2fc506fd613a05d13f01bb436e
Adam Kerr <adamrkerr@gmail.com>
2019-02-23 17:55:28 -0800

    too cold to build a fire

--- src/client/index.js
+++ src/client/index.js
@@ -80,8 +80,8 @@
 
     function newMessage(message) {
         hereApi.setMapLocation(message.longitude, message.latitude);
-        currentLocation.longitude = message.longitude;
-        currentLocation.latitude = message.latitude;
+        currentLocation.lng = message.longitude;
+        currentLocation.lat = message.latitude;
     }
 
 })();
