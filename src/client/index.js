85ea922de90d9716665bf27b0c055d94a94e6f13
Adam Kerr <adamrkerr@gmail.com>
2019-02-23 17:50:42 -0800

    I'M GONNA SEND IT!!!!

--- src/client/index.js
+++ src/client/index.js
@@ -6,24 +6,80 @@
     $('#north_button').click(() => {
         currentLocation.lat += .00005;
 
-        hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
+        sendLocation(currentLocation.lat, currentLocation.lng);
+        //connection.send('location', {latitude:currentLocation.lat, longitude:currentLocation.lng});
+        //hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
     });
 
     $('#west_button').click(() => {
         currentLocation.lng += -.00005;
 
-        hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
+        sendLocation(currentLocation.lat, currentLocation.lng);
+        //hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
     });
 
     $('#east_button').click(() => {
         currentLocation.lng += .00005;
 
-        hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
+        sendLocation(currentLocation.lat, currentLocation.lng);
+        //hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
     });
 
     $('#south_button').click(() => {
         currentLocation.lat += -.00005;
 
-        hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
+        sendLocation(currentLocation.lat, currentLocation.lng);
+        //hereApi.setMapLocation(currentLocation.lng, currentLocation.lat);
     });
+    
+    var apiBaseUrl = 'https://devweek2019-robotservice.azurewebsites.net';
+    var connection;
+
+    getConnectionInfo().then(info => {
+        // make compatible with old and new SignalRConnectionInfo
+        info.accessToken = info.accessToken || info.accessKey;
+        info.url = info.url || info.endpoint;
+
+        const options = {
+            accessTokenFactory: () => info.accessToken
+        };
+
+        connection = new signalR.HubConnectionBuilder()
+            .withUrl(info.url, options)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        connection.on('newLocation', newMessage);
+        connection.onclose(() => console.log('disconnected'));
+
+        console.log('connecting...');
+        connection.start()
+            .then(() => console.log('connected!'))
+            .catch(console.error);
+
+    }).catch(alert);
+
+    function getAxiosConfig() {
+        const config = {
+            headers: {}
+        };
+        return config;
+    }
+
+    function getConnectionInfo() {
+        return axios.post(`${apiBaseUrl}/api/negotiate`, null, getAxiosConfig())
+            .then(resp => resp.data);
+    }
+
+    function sendLocation(lat, lng) {
+        return axios.post(`${apiBaseUrl}/api/location`, {
+            latitude: lat,
+            longitude: lng        
+        }, getAxiosConfig()).then(resp => resp.data);
+    }
+
+    function newMessage(message) {
+        hereApi.setMapLocation(message.longitude, message.latitude);
+    }
+
 })();
---
+++ src/location_service/LocationService/LocationReading.cs
@@ -0,0 +1,4 @@
+public class LocationReading{
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+}
